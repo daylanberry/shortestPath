5767cee3fdeba125812f453ae63bd536db0d93c0
Daylan <daylanberry@gmail.com>
2020-03-19 16:01:12 -0700

    created tests for shortest path

--- src/components/Map.test.js
+++ src/components/Map.test.js
@@ -7,3 +7,7 @@ it ('should return the correct snapshot of Map component', () => {
   const mapComponent = renderer.create(<Map/>).toJSON()
   expect(mapComponent).toMatchSnapshot()
 })
+
+it ('test', () => {
+  expect(1 + 1).toEqual(2)
+})
